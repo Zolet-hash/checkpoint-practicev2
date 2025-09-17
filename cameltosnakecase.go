f3dea97dcf16d07ee83edd81df39599272829a1a
zolet-hash <emmsnuelsulwe@gmail.com>
2025-09-17 15:30:36 +0300

    add cameltosnakecase.go file

--- cameltosnakecase.go
+++ cameltosnakecase.go
@@ -13,17 +13,37 @@ func main() {
 	fmt.Println(CamelToSnakeCase("hey2"))
 }
 
-func CamelToSnakeCase(s string) string{
+func CamelToSnakeCase(s string) string {
 	if s == "" {
 		return s
 	}
 	for i, r := range s {
-		if r >= 0 && r <= 9 {
+		if r >= '0' && r <= '9' {
 			return s
 		}
-		if !(r >= 'a' && r <= 'z' || r <= 'A' && r >= 'Z') {
+		if !(r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z') {
 			return s
 		}
-		
+		if i > 0 && r >= 'A' && r <= 'Z' {
+			prev := rune(i-1)
+
+			if prev >= 'A' && prev <= 'Z' {
+				return s
+			}
+		}
+		last := rune(s[len(s)-1])
+
+		if last >= 'A' && last <= 'Z' {
+			return s
+		}
 	}
+
+	result := []rune{}
+	for i, r := range s {
+		if i > 0 && r >= 'A' && r <= 'Z' {
+			result = append(result, '_')
+		}
+		result = append(result, r)
+	}
+	return string(result)
 }
